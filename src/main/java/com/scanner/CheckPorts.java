502e4fdbdd6951d5443e249e984d9d7915696bfd
TSkrebe <titas.skrebe@gmail.com>
2015-07-29 17:16:15 +0300

    Second commit

---
+++ src/main/java/com/scanner/CheckPorts.java
@@ -0,0 +1,73 @@
+package com.scanner;
+
+import java.io.IOException;
+import java.lang.Integer;
+import java.lang.InterruptedException;
+import java.lang.String;
+import java.lang.System;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CheckPorts {
+
+    private final String ip;
+    private final int port1;
+    private final int port2;
+    private final int threads;
+    private final int timeout;
+
+    public CheckPorts(String ip, int port1, int port2, int threads, int timeout) {
+        this.ip = ip;
+        this.port1 = port1;
+        this.port2 = port2;
+        this.threads = threads;
+        this.timeout = timeout;
+    }
+
+
+    public void run() {
+        ExecutorService executorService = Executors.newFixedThreadPool(threads);
+        List<Callable<Integer>> list = new ArrayList<>();
+
+        for (int i = port1; i <= port2; i++) {
+            final int port = i;
+            list.add(() -> {
+                try (Socket socket = new Socket()){
+                    socket.connect(new InetSocketAddress(ip, port), timeout);
+                    return port;
+                } catch (IOException e) {
+                    return -1;
+                }
+            });
+        }
+
+        int all = 0;
+        try {
+            List<Future<Integer> >  futures = executorService.invokeAll(list);
+            executorService.shutdown();
+            for (Future<Integer> f : futures){
+                int p = f.get();
+                if (p != -1) {
+                    all++;
+                    System.out.println(p);
+                }
+
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(all + " open port(s) in total.");
+    }
+
+}
---
+++ src/main/java/com/scanner/Main.java
@@ -0,0 +1,80 @@
+package com.scanner;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;import java.lang.String;import java.lang.System;
+
+
+/** IPv4
+ * -i for defining ip address
+ * -p for defining port
+ * -p2 for defining range default same as p
+ * -t number of threads to use for connections   default 10
+ * -o timeOut limit in millis. default 1000ms range 0 - 10000
+ */
+
+public class Main {
+
+    @Option(name = "-i", usage = "host", required = true)
+    private String ARG_IP;
+    @Option(name = "-p", usage = "port1", required = true)
+    private int ARG_PORT;
+    @Option(name = "-p2", usage = "port2")
+    private int ARG_PORT2 = -1;
+    @Option(name = "-t", usage = "threads")
+    private int ARG_THREADS = 50;
+    @Option(name = "-o", usage = "timeout")
+    private int ARG_TIMEOUT = 1000;
+
+
+    private static final int MIN_PORT = 0;
+    private static final int MAX_PORT = 65535;
+
+    private static final int MIN_THREADS = 1;
+
+    private static final int MIN_TIMEOUT = 0;
+
+
+    public static void main(String[] args) {
+        new Main().doMain(args);
+
+    }
+
+    private void doMain(String[] args) {
+        int d;
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+            if (ARG_PORT < MIN_PORT || ARG_PORT > MAX_PORT)
+                throw new CmdLineException(parser, "Port has to be between " + MIN_PORT + " and " + MAX_PORT);
+            if (ARG_PORT2 != -1 && (ARG_PORT2 < MIN_PORT || ARG_PORT2 > MAX_PORT || ARG_PORT2 < ARG_PORT))
+                throw new CmdLineException("Port has to be between " + ARG_PORT + " and " + MAX_PORT);
+            if (ARG_THREADS < MIN_THREADS)
+                throw new CmdLineException("Number of threads has to be bigger than 0");
+            if (ARG_TIMEOUT < MIN_TIMEOUT)
+                throw new CmdLineException("Time out has to be bigger than -1");
+
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("java SampleMain [options...] arguments...");
+            // print the list of available options
+            parser.printUsage(System.err);
+            System.err.println();
+            return;
+        }
+
+        if (ARG_PORT2 == -1)
+            ARG_PORT2 = ARG_PORT;
+
+
+   //     System.out.print(ARG_IP + " " + ARG_PORT + " " + ARG_PORT2 + " " + ARG_THREADS + " " + ARG_TIMEOUT);
+
+        System.out.println("Estimated time left: " + ((ARG_PORT2-ARG_PORT+1)/ARG_THREADS)*(ARG_TIMEOUT/1000) + " sec.");
+
+        //if everything is ok run through ports
+        CheckPorts checkPorts = new CheckPorts(ARG_IP, ARG_PORT, ARG_PORT2, ARG_THREADS, ARG_TIMEOUT);
+        checkPorts.run();
+    }
+
+
+}
